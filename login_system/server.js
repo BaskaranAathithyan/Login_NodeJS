89eeb5d95464670d17e29356376bbc9c127f5012
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-01-29 21:37:14 +0530

    backend

--- login_system/server.js
+++ login_system/server.js
@@ -1,13 +1,26 @@
 const express=require('express');
 const path=require("path");
 const app=express();
+const bodyparser=require("body-parser");
+const session=require("express-session");
+
 
 const port= process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}))
 
 app.set('view engine','ejs');
 
 //load static assest
 app.use('/static',express.static(path.join(__dirname,'public')))
+
+
+app.use(session({
+    secret:'secret',
+    resave:false,
+    saveUninitializeed:true
+}))
 
 //home route
 app.get('/',(req,res) => {
