edc154c23f5aaeb282b4993c35957abf329565f2
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-02-01 09:17:42 +0530

    test

--- login_system/router.js
+++ login_system/router.js
@@ -10,11 +10,34 @@ const credential={
 router.post('/login',(req,res)=>{
     if(req.body.email==credential.email && req.body.password== credential.password){
         req.session.user=req.body.email;
-        //res.redirect('/dashboard');
-        res.end("login successful");
+        res.redirect('/route/dashboard');
+        //res.end("login successful");
     }else{
         res.end("invalid Username");
     }
 });
+
+
+//route for dashboard
+router.get("/dashboard",(req,res) => {
+    if(req.session.user){
+        res.render('dashboard',{user:req.session.user})
+    }else{
+        res.send("Unauthorize user");
+    }
+})
+
+
+//route for logout
+router.get('/logout',(req,res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error");
+        }else{
+            res.render('base',{title:"Express",logout:"logout Successfully"})
+        }
+    })
+})
 
 module.exports=router;
